364660af46de368507a7d60b781e8f25a121f31c
C. Saxby Bain <csaxbybain@yahoo.com>
2026-02-21 13:51:45 -0500

    Formatted code

--- js/script.js
+++ js/script.js
@@ -2,27 +2,17 @@ var response = document.querySelector(".response");
 var showGuess = document.querySelector(".show-guess");
 const button = document.querySelector("button");
 const number = document.querySelector(".number");
-
-
-
-
 let randomNumber = Math.floor(Math.random() * 27);
-
 const num = number.value;
 
 
-
-const playGame = function(num) {
-
-
-if (num === randomNumber) {
-  response.innerText = "Hey, you totally guessed the number! Great job!";
-
-
-} else if (num !== randomNumber) {
-  response.innerText = `Sorry. Your guess is incorrect. The number is ${randomNumber}.`;
-}
-    else if (num > 26) {
+const playGame = function (num) {
+  if (num === randomNumber) {
+    response.innerText = "Hey, you totally guessed the number! Great job!";
+  } else if (num !== randomNumber) {
+    response.innerText = `Sorry. Your guess is incorrect. The number is ${randomNumber}.`;
+  }
+  else if (num > 26) {
     response.innerText = "We need a number between 1 and 26.";
     return;
   } else if (num < 1) {
@@ -31,7 +21,6 @@ if (num === randomNumber) {
   } else {
     response.innerText = "That's not a number. Please try again.";
   }
-
   showGuess.innerText = randomNumber;
 }
 
